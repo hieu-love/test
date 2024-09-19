b4287bf3a3a289e5f6cb3abd83b55d4a3af7f55c
Le Khac Hieu <lekhachieupt@gmail.com>
2024-09-19 16:22:13 +0700

    them bai2 vao master

---
+++ bai2.java
@@ -0,0 +1,17 @@
+package tuan2;
+
+public class bai2 {
+    public static void main(String[] args) {
+        // Khai báo các biến
+        String name = "My Duyen"; 
+        int age = 19;             
+        double height = 1.55;     
+        boolean like = true; 
+
+        // In ra các giá trị đã lưu
+        System.out.println("Ten: " + name);
+        System.out.println("Tuoi: " + age);
+        System.out.println("Chieu cao: " + height);
+        System.out.println("Co thich lap trinh khong: " + (like ? "Co" : "Khong"));
+    }
+}
